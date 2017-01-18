0a80b5847e26790d58686bac1023082fb7bf5ed1
Magdalena <magdalena.joanna.ostrowska@gmail.com>
2017-01-18 20:34:45 +0100

    Pierwszy commit.

---
+++ listy.py
@@ -0,0 +1,15 @@
+B = list(range(0, 101, 2))
+print B
+
+Lista = [1, 4, 8, 9, -1, 0, 2]
+print (4 in Lista)
+print Lista[0:7]
+print sorted(Lista[0:7])
+
+print sorted(Lista, reverse=True)
+
+for i in range(0, 20, 3):
+    print i
+
+A =('A', 10, 3)
+print A
